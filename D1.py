99baeaaaf49a270c342b9a055cc85cce0e6c1583
PratyushPoddar07 <shuvampoddar018@gmail.com>
2024-11-16 00:44:49 +0530

    O(n)

--- D1.py
+++ D1.py
@@ -13,3 +13,29 @@ def rotate(nums,int) -> None:
     return nums
 
 print(rotate(arr,2))
+
+
+# better way and more optimize as O(n)
+"""
+class Solution(object):
+    def rotate(self, nums, k):
+       
+        n = len(nums)
+        k= k%n
+
+        def reverse(start,end):
+            while start < end:
+                nums[start],nums[end]= nums[end],nums[start]
+                start +=1
+                end -=1
+
+        reverse(0,n-1)
+        reverse(0,k-1)
+        reverse(k,n-1)
+
+        return nums
+
+
+    
+        
+"""
