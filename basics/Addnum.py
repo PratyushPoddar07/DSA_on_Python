db651e42dba75ebbca9055552d28983ee7d2ec2c
PratyushPoddar07 <shuvampoddar018@gmail.com>
2024-12-28 23:55:42 +0530

    add

--- basics/Addnum.py
+++ basics/Addnum.py
@@ -1,6 +1,24 @@
 num1 = int(input("Enter num1:   "))
 num2 = int(input("Enter num2:   "))
 
-sum = num1+num2
+# sum = num1+num2
 
-print(sum)
+# print(sum)
+
+# --FUNCTION--
+
+# def ADD(num1,num2):
+#     return num1+num2
+
+# print(ADD(num1,num2))
+
+# --RECURSIVELY--
+def add_recursive(x,y):
+    if y ==0:
+        return x
+    else:
+        return add_recursive(x+1,y-1)
+    
+
+result = add_recursive(num1,num2)
+print(result)
