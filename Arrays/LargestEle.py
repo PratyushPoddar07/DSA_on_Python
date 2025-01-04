da206d9b24e3f606caa15cfac6e0c913a295b873
PratyushPoddar07 <shuvampoddar018@gmail.com>
2025-01-04 13:27:57 +0530

    in-built max method()

--- Arrays/LargestEle.py
+++ Arrays/LargestEle.py
@@ -9,3 +9,14 @@ def findMax(arr):
     return max 
 arr = {10,20,4}
 print(f"max: {findMax(arr)}")        
+
+
+# one more trick
+def largest(arr):
+   ans = max(arr)
+   return ans
+
+if __name__ == '__main__':
+    arr2 = list(map(int,input("Enter the element by space: ").split()))
+    n= len(arr2)
+    print(f"Largest element is: {largest(arr2)} having {n} element")
